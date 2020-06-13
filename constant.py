477ddf2c54bd928b89b410ad4129f0d8340af002
Karn <karndhami@gmail.com>
2020-06-12 18:37:42 -0600

    minor cleanup; more todo

---
+++ constant.py
@@ -0,0 +1,4 @@
+ERROR_STR = '\nError: '
+ACTIONS_STR = '\nAction: '
+DIVIDER_STR = '\n-------------------'
+CHOICE_STR = '\nYour Choice: '
--- textAdventure.py
+++ textAdventure.py
@@ -1,68 +1,85 @@
+import constant
 
-#Input Validation
+# Input Validation
+
+
 def input_valid1(choice):
-	if choice in ('1'):
-		input_needed = False
-	else:
-		print('\nERROR:', choice, 'is not a valid choice. Select 1.\n')
+    if choice in ('1'):
+        input_needed = False
+    else:
+		print(constant.ERROR_STR, choice, 'is not a valid choice. Select 1.\n')
+		# print('\nERROR:', choice, 'is not a valid choice. Select 1.\n')
 		input_needed = True
-	return choice
+    return choice
+
+
 def input_valid2(choice):
-	if choice in ('1','2'):
-		input_needed = False
-	else:
-		print('\nERROR:', choice, 'is not a valid choice. Select 1, or 2.\n')
+    if choice in ('1', '2'):
+        input_needed = False
+    else:
+		print(constant.ERROR_STR, choice, 'is not a valid choice. Select 1, or 2.\n')
+		# print('\nERROR:', choice, 'is not a valid choice. Select 1, or 2.\n')
 		input_needed = True
-	return choice
+    return choice
+
+
 def input_valid3(choice):
-	if choice in ('1','2','3'):
-		input_needed = False
-	else:
-		print('\nERROR:', choice, 'is not a valid choice. Select 1, 2, or 3.\n')
+    if choice in ('1', '2', '3'):
+        input_needed = False
+    else:
+		print(constant.ERROR_STR, choice, 'is not a valid choice. Select 1, 2, or 3.\n')
+		# print('\nERROR:', choice, 'is not a valid choice. Select 1, 2, or 3.\n')
 		input_needed = True
-	return choice
+    return choice
+
+
 def input_valid4(choice):
-	if choice in ('1','2','3','4'):
-		input_needed = False
-	else:
-		print('\nERROR:', choice, 'is not a valid choice. Select 1, 2, 3, or 4.\n')
+    if choice in ('1', '2', '3', '4'):
+        input_needed = False
+    else:
+		print(constant.ERROR_STR, choice, 'is not a valid choice. Select 1, 2, 3, or 4.\n')
+		# print('\nERROR:', choice, 'is not a valid choice. Select 1, 2, 3, or 4.\n')
 		input_needed = True
-	return choice
+    return choice
 
-#Kitchen
+# Kitchen
+
+
 def kitchen(door_locked, current_dial, current_lever):
-	input_needed = True
-	while input_needed:
-		print('\nRoom: Kitchen'+'\n-------------------')
-		print('You are in the Kitchen which has all kinds of appliances to prepare food with. You see in front of you a lever that can be pushed forward or pulled back. Behind you is the door back to the entrance room.')
-		print('\n- The lever is currently set to', current_lever, '-')
-		print('Actions:'+'\n1) Push the lever forward'+'\n2) Pull the lever back'+'\n3) Go back to the Entrance Room')
-		choice = input('\nYour choice:')
-		choice = input_valid3(choice)
-		
-		if choice == '1':
-			current_lever = 'forward'
-			door_locked = True
-			input_needed = True
-		elif choice == '2':
-			current_lever = 'back'
-			door_locked = False
-			input_needed = True
-		elif choice == '3':
-			input_needed = False
-	return current_lever
+    input_needed = True
+    while input_needed:
+        print('\nRoom: Kitchen'+ constant.DIVIDER_STR)
+        print('You are in the Kitchen which has all kinds of appliances to prepare food with. You see in front of you a lever that can be pushed forward or pulled back. Behind you is the door back to the entrance room.')
+        print('\n- The lever is currently set to', current_lever, '-')
+        print('Actions:'+'\n1) Push the lever forward' + '\n2) Pull the lever back'+'\n3) Go back to the Entrance Room')
+        choice = input(constant.CHOICE_STR)
+        choice = input_valid3(choice)
 
-#Pantry
+        if choice == '1':
+            current_lever = 'forward'
+            door_locked = True
+            input_needed = True
+        elif choice == '2':
+            current_lever = 'back'
+            door_locked = False
+            input_needed = True
+        elif choice == '3':
+            input_needed = False
+    return current_lever
+
+# Pantry
+
+
 def pantry(door_locked, current_dial, current_lever):
 	input_needed = True
+	print('\nRoom: Pantry'+constant.DIVIDER_STR)
+	print('You are in the Pantry which contains all kinds of food. You see in front of you a dial that you can turn to 3 possible settings: blue, red, and green. Behind you is the door back to the entrance room.')
 	while input_needed:
-		print('\nRoom: Pantry'+'\n-------------------')
-		print('You are in the Pantry which contains all kinds of food. You see in front of you a dial that you can turn to 3 possible settings: blue, red, and green. Behind you is the door back to the entrance room.')
 		print('\n- The dial is currently set to', current_dial, '-')
-		print('Actions:'+'\n1) Turn the dial to blue'+'\n2) Turn the dial to red'+'\n3) Turn the dial to green'+'\n4) Go back to the Entrance Room')
-		choice = input('Your choice:')
+		print(constant.ACTIONS_STR +'\n1) Turn the dial to blue'+'\n2) Turn the dial to red' + '\n3) Turn the dial to green'+'\n4) Go back to the Entrance Room')
+		choice = input(constant.CHOICE_STR)
 		choice = input_valid4(choice)
-		
+
 		if choice == '1':
 			current_dial = 'blue'
 			door_locked = True
@@ -79,18 +96,20 @@ def pantry(door_locked, current_dial, current_lever):
 			input_needed = False
 	return current_dial
 
-#Entrance	
+# Entrance
+
+
 def entryway(door_locked, current_dial, current_lever):
+	print('\nRoom: Entrance Room'+constant.DIVIDER_STR)
+	print('In front of you is a door that leads to the rest of the house. To your left is the Kitchen. To your right is the Pantry')
 	while door_locked == True:
+		print(constant.ACTIONS_STR +'\n1) Try to open the door' +'\n2) Go to your left and into the Kitchen'+'\n3) Go to your right and into the Pantry')
 		input_needed = True
-		print('\nRoom: Entrance Room'+'\n-------------------')
-		print('In front of you is a door that leads to the rest of the house. To your left is the Kitchen. To your right is the Pantry')
-		print('\nActions:'+'\n1) Try to open the door'+'\n2) Go to your left and into the Kitchen'+'\n3) Go to your right and into the Pantry')
-		choice = input('\nYour choice:')
+		choice = input(constant.CHOICE_STR)
 		choice = input_valid3(choice)
-		
+
 		if choice == '1':
-			if  (current_lever == 'back') and (current_dial == 'red'):
+			if (current_lever == 'back') and (current_dial == 'red'):
 				door_locked = False
 			else:
 				print('\n- The door is locked -')
@@ -99,24 +118,26 @@ def entryway(door_locked, current_dial, current_lever):
 		elif choice == '3':
 			current_dial = pantry(door_locked, current_dial, current_lever)
 	return door_locked
-	
-#Attic
+
+# Attic
+
+
 def attic(dry_soil, current_soil, ball_of_string, has_cheese):
 	input_needed = True
+	print('\nRoom: Attic'+constant.DIVIDER_STR)
+	print('You have entered the Attic. There is what seems to be an unlimited supply of cheese up here. How strange you think to yourself. There is also a small hole in the attic floor, too small for any of the cheese to fit through.')
 	while input_needed:
-		print('\nRoom: Attic'+'\n-------------------')
-		print('You have entered the Attic. There is what seems to be an unlimited supply of cheese up here. How strange you think to yourself. There is also a small hole in the attic floor, too small for any of the cheese to fit through.')
 		if ball_of_string == 2:
-			print('\nActions:'+'\n1) Go down the stairs back to the living room'+'\n2) Try to drop some cheese down the hole'+'\n3) Take some cheese'+'\n4) Drop the ball of string down the hole')
-			choice = input('\nYour choice:')
+			print(constant.ACTIONS_STR+'\n1) Go down the stairs back to the living room' + '\n2) Try to drop some cheese down the hole'+'\n3) Take some cheese'+'\n4) Drop the ball of string down the hole')
+			choice = input(constant.CHOICE_STR)
 			choice = input_valid4(choice)
 			option4 = True
 		else:
-			print('\nActions:'+'\n1) Go down the stairs back to the living room'+'\n2) Try to drop some cheese down the hole'+'\n3) Take some cheese')
-			choice = input('\nYour choice:')
+			print(constant.ACTIONS_STR+'\n1) Go down the stairs back to the living room' + '\n2) Try to drop some cheese down the hole'+'\n3) Take some cheese')
+			choice = input(constant.CHOICE_STR)
 			choice = input_valid3(choice)
 			option4 = False
-		
+
 		if choice == '1':
 			input_needed = False
 		elif choice == '2':
@@ -126,110 +147,123 @@ def attic(dry_soil, current_soil, ball_of_string, has_cheese):
 				print('The cheese is too big to drop down the hole')
 			input_needed = True
 		elif choice == '3':
-			has_cheese +=1
+			has_cheese += 1
 			print('You acquired some cheese.')
 			input_needed = True
-		elif (choice == '4') and (option4==True):
+		elif (choice == '4') and (option4 == True):
 			ball_of_string = 0
 			print('Heads up down there. You dropped the string down the hole.')
 			input_needed = True
 	return ball_of_string, has_cheese
 
-#Bedroom
+# Bedroom
+
+
 def bedroom(dry_soil, current_soil, ball_of_string, has_cheese):
-	input_needed = True
-	while input_needed:
-		print('\nRoom: Bedroom'+'\n-------------------')
-		if ball_of_string == 2:
-			print('You have entered the Bedroom. Look how comfortable the bed looks. But there\'s no time to take a nap. You notice a black tomcat intently watching a mouse hole.')
-			print('\nActions:'+'\n1) Go back to the Living Room'+'\n2) Dangle the string in front of the cat')
-			choice = input('\nYour choice:')
-			choice = input_valid2(choice)
-			option2 = True
-		elif (ball_of_string == 1) or (has_cheese == 0):
-			print('You have entered the Bedroom. Look how comfortable the bed looks. But there\'s no time to take a nap. You notice a black tomcat intently watching a mouse hole.')
-			print('\nActions:'+'\n1) Go back to the Living Room')
-			choice = input('\nYour choice:')
-			choice = input_valid1(choice)
-			option2 = False
-		else:
-			print('You have entered the Bedroom. Look how comfortable the bed looks. But there\'s no time to take a nap. You see a mouse in front of the mouse hole.')
-			print('\nActions:'+'\n1) Go back to the Living Room'+'\n2) Give some cheese to the mouse')
-			choice = input('\nYour choice:')
-			choice = input_valid2(choice)
-			option2 = True
-		
-		if choice == '1':
-			input_needed = False
-		elif (choice == '2') and (option2==True):
-			if ball_of_string==2:
-				print('The cat turns it\'s head towards you. The mouse hole is more important to the cat. It ignores you')
-			elif (ball_of_string==0) and (has_cheese!=0):
-				print('Looks like the mouse is a fan of the cheese.')
-				current_soil = 'fertilized'
-				has_cheese -= 1
-			input_needed = True
-	return current_soil
+    input_needed = True
+    while input_needed:
+        print('\nRoom: Bedroom'+constant.DIVIDER_STR)
+        if ball_of_string == 2:
+            print('You have entered the Bedroom. Look how comfortable the bed looks. But there\'s no time to take a nap. You notice a black tomcat intently watching a mouse hole.')
+            print(constant.ACTIONS_STR + '\n1) Go back to the Living Room' + '\n2) Dangle the string in front of the cat')
+            choice = input(constant.CHOICE_STR)
+            choice = input_valid2(choice)
+            option2 = True
+        elif (ball_of_string == 1) or (has_cheese == 0):
+            print('You have entered the Bedroom. Look how comfortable the bed looks. But there\'s no time to take a nap. You notice a black tomcat intently watching a mouse hole.')
+            print(constant.ACTIONS_STR + '\n1) Go back to the Living Room')
+            choice = input(constant.CHOICE_STR)
+            choice = input_valid1(choice)
+            option2 = False
+        else:
+            print('You have entered the Bedroom. Look how comfortable the bed looks. But there\'s no time to take a nap. You see a mouse in front of the mouse hole.')
+            print(constant.ACTIONS_STR + '\n1) Go back to the Living Room' + '\n2) Give some cheese to the mouse')
+            choice = input(constant.CHOICE_STR)
+            choice = input_valid2(choice)
+            option2 = True
 
-#Living Room
+        if choice == '1':
+            input_needed = False
+        elif (choice == '2') and (option2 == True):
+            if ball_of_string == 2:
+                print('The cat turns it\'s head towards you. The mouse hole is more important to the cat. It ignores you')
+            elif (ball_of_string == 0) and (has_cheese != 0):
+                print('Looks like the mouse is a fan of the cheese.')
+                current_soil = 'fertilized'
+                has_cheese -= 1
+            input_needed = True
+    return current_soil
+
+# Living Room
+
+
 def living_room(dry_soil, current_soil, ball_of_string, has_cheese):
-	while dry_soil==True:
-		input_needed = True
-		print('\nRoom: Living Room'+'\n-------------------')
-		if ball_of_string==1:
-			print('You have entered the Living Room of the house. The decoration is not to your taste, but neither is having doors dissapear behind you trapping you in a weird house. In the Living Room you see:'+'\n* A pot of soil'+'\n* Stairs going up'+'\n* A dark doorway'+'\n* A ball of string')
-			print('\nActions:'+'\n1) View the pot of Soil'+'\n2) Go up the stairs'+'\n3) Go through the dark doorway'+'\n4) Take the ball of string')
-			choice = input('\nYour choice:')
-			choice = input_valid4(choice)
-			option4 = True
-		else:
-			print('You have entered the Living Room of the house. The decoration is not to your taste, but neither is having doors dissapear behind you trapping you in a weird house. In the Living Room you see:'+'\n* A pot of soil'+'\n* Stairs going up'+'\n* A dark doorway')
-			print('\nActions:'+'\n1) View the pot of Soil'+'\n2) Go up the stairs'+'\n3) Go through the dark doorway')
-			choice = input('\nYour choice:')
-			choice = input_valid3(choice)
-			option4 = False
-		
-		if choice == '1':
-			if (current_soil == 'fertilized'):
-				dry_soil = False
-				print('\nDONE')
-			else:
-				print('\nThe soil looks dry')
-		elif choice == '2':
-			ball_of_string, has_cheese = attic(dry_soil, current_soil, ball_of_string, has_cheese)
-		elif choice == '3':
-			current_soil = bedroom(dry_soil, current_soil, ball_of_string, has_cheese)
-		elif (choice == '4') and (option4==True):
-			ball_of_string = 2
-			input_needed = True
-	return dry_soil	
+    while dry_soil == True:
+        input_needed = True
+        print('\nRoom: Living Room'+'\n-------------------')
+        if ball_of_string == 1:
+            print('You have entered the Living Room of the house. The decoration is not to your taste, but neither is having doors dissapear behind you trapping you in a weird house. In the Living Room you see:' + '\n* A pot of soil'+'\n* Stairs going up'+'\n* A dark doorway'+'\n* A ball of string')
+            print(constant.ACTIONS_STR + '\n1) View the pot of Soil'+'\n2) Go up the stairs' + '\n3) Go through the dark doorway'+'\n4) Take the ball of string')
+            choice = input(constant.CHOICE_STR)
+            choice = input_valid4(choice)
+            option4 = True
+        else:
+            print('You have entered the Living Room of the house. The decoration is not to your taste, but neither is having doors dissapear behind you trapping you in a weird house. In the Living Room you see:' + '\n* A pot of soil'+'\n* Stairs going up'+'\n* A dark doorway')
+            print(constant.ACTIONS_STR + '\n1) View the pot of Soil' + '\n2) Go up the stairs'+'\n3) Go through the dark doorway')
+            choice = input(constant.CHOICE_STR)
+            choice = input_valid3(choice)
+            option4 = False
 
-#Part 1: Entrance, Kitchen, Pantry
+        if choice == '1':
+            if (current_soil == 'fertilized'):
+                dry_soil = False
+                print('\nDONE')
+            else:
+                print('\nThe soil looks dry')
+        elif choice == '2':
+            ball_of_string, has_cheese = attic(
+                dry_soil, current_soil, ball_of_string, has_cheese)
+        elif choice == '3':
+            current_soil = bedroom(
+                dry_soil, current_soil, ball_of_string, has_cheese)
+        elif (choice == '4') and (option4 == True):
+            ball_of_string = 2
+            input_needed = True
+    return dry_soil
+
+# Part 1: Entrance, Kitchen, Pantry
+
+
 def part1():
-	door_locked = True
-	current_dial = 'blue'
-	current_lever = 'forward'
-	
-	print('You have just come from outside and into the Entrance Room. The door that you used has just disappeared.')
-	
-	door_locked = entryway(door_locked, current_dial, current_lever)
-	
-	print('\nCONGRATULATIONS!!!'+'\nYou opened the door. You can now enter the rest of the house.')
+    door_locked = True
+    current_dial = 'blue'
+    current_lever = 'forward'
 
-#Part 2: Living Room, Attic, Bedroom
+    print('You have just come from outside and into the Entrance Room. The door that you used has just disappeared.')
+
+    door_locked = entryway(door_locked, current_dial, current_lever)
+
+    print('\nCONGRATULATIONS!!!' + '\nYou opened the door. You can now enter the rest of the house.')
+
+# Part 2: Living Room, Attic, Bedroom
+
+
 def part2():
-	dry_soil = True
-	current_soil = 'dry'
-	ball_of_string = 1
-	has_cheese = 0
-	
-	print('\nYou turn around and see that the Entrance Room door has also disappeared. It looks like you\'ll be trapped in this house forever. Might as well explore what\'s in it.')
+    dry_soil = True
+    current_soil = 'dry'
+    ball_of_string = 1
+    has_cheese = 0
 
-	dry_soil = living_room(dry_soil, current_soil, ball_of_string, has_cheese)
+    print('\nYou turn around and see that the Entrance Room door has also disappeared. It looks like you\'ll be trapped in this house forever. Might as well explore what\'s in it.')
 
-	print('\nIt looks like there is a vine growing from the soil. How did that happen. Oh well, doesn\'t matter now because for some crazy reason you can finally get out of the house'+'... ... ...'+'\nCONGRATULATIONS!!!'+'\nAll that needed to happen was for a vine to somehow magically grow from the soil.')
+    dry_soil = living_room(dry_soil, current_soil, ball_of_string, has_cheese)
+
+    print('\nIt looks like there is a vine growing from the soil. How did that happen. Oh well, doesn\'t matter now because for some crazy reason you can finally get out of the house' + '... ... ...'+'\nCONGRATULATIONS!!!'+'\nAll that needed to happen was for a vine to somehow magically grow from the soil.')
+
 
 def main():
-	part1()
-	part2()
+    part1()
+    part2()
+
+
 main()
